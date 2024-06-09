1de056ff2261d0dbc54cd12a3fb3e3366cb53a8a
rahildhodapkar <rdgms10@gmail.com>
2024-06-09 14:58:40 -0400

    Set up Ship class
    Game needs ships that can track whether they are sunk or not

--- src/classes/ship.js
+++ src/classes/ship.js
@@ -1,3 +1,39 @@
 export class Ship {
   #length;
+  // Number of times ship has been hit
+  #numHits;
+  #sunk;
+
+  constructor(length) {
+    this.#length = length;
+    this.#numHits = 0;
+    this.#sunk = false;
+  }
+
+  hit() {
+    // If ship is already sunk, cannot get hit more
+    if (this.#sunk) {
+      return;
+    }
+
+    this.#numHits++;
+  }
+
+  getHits() {
+    return this.#numHits;
+  }
+
+  isSunk() {
+    if (!this.#sunk && this.#length === this.#numHits) {
+      this.#sunk = true;
+    }
+
+    return this.#sunk;
+  }
+
+  toString() {
+    return `length: ${this.#length}, number of hits: ${
+      this.#numHits
+    }, is sunk: ${this.#sunk}`;
+  }
 }
